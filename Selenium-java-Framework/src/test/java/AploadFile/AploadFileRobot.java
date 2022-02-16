80b2c0addfd01112422d35debba2adb10b0f1d1e
Fodil <fodil.laid@yahoo.fr>
2022-02-16 12:44:46 -0500

    First Commit - Added project

---
+++ Selenium-java-Framework/src/test/java/AploadFile/AploadFileRobot.java
@@ -0,0 +1,72 @@
+package AploadFile;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+
+
+public class AploadFileRobot {
+
+	public static void main(String[] args) throws InterruptedException, IOException, AWTException {
+		String downloadPath=System.getProperty("user.dir");
+		HashMap chromePrefs = new HashMap(); 
+		chromePrefs.put("profile.default_content_settings.popups", 0); 
+		chromePrefs.put("download.default_directory", downloadPath); 
+		ChromeOptions options=new ChromeOptions(); 
+		options.setExperimentalOption("prefs", chromePrefs); 
+		WebDriver driver = new ChromeDriver(options);
+        //driver.get("https://www.sodapdf.com/fr/pdf-en-jpg/");
+        driver.get("https://www.ilovepdf.com/fr"); 
+        driver.manage().window().maximize();
+        //driver.findElement(By.xpath("//label[@class='btn-choose']")).click();
+        driver.findElement(By.xpath("//h3[text()='PDF en JPG']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//span[text()='Sélectionner les fichiers PDF']")).click();
+		Thread.sleep(3000);
+        
+		Robot robot = new Robot();
+        StringSelection filePath = new StringSelection("C:\\Users\\Fodil\\OneDrive - Levio\\Levio\\AutoIt\\Test PDF.pdf");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(filePath, null);
+        //Copier
+		robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        //Enlever les touches
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        
+        //Cliquer sur le bouton Entrée
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        
+        WebDriverWait wait=new WebDriverWait(driver,10); 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='processTask']")));
+        driver.findElement(By.xpath("//button[@id='processTask']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='pickfiles']")));
+        Thread.sleep(5000); 
+        File f=new File(downloadPath+"/ilovepdf_pages-to-jpg.zip"); 
+        if(f.exists()) 
+        { 
+        	Assert.assertTrue(f.exists()); 
+        	//f.delete(); 
+        	
+        	} 
+        } 
+}
+
+
+
+
---
+++ Selenium-java-Framework/src/test/java/BrowserTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+	}
+
+}
